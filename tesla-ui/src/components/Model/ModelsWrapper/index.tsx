1305eee1359197a04249248d8f48f4cb364a87bd
Rafael Matos <rafaelmatostj@gmail.com>
2020-11-09 15:50:14 -0300

    init

---
+++ tesla-ui/src/components/Model/ModelsWrapper/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const ModelsWrapper: React.FC = () => {
+    return(
+        <Container>
+            <h1>ModelsWrapper</h1>
+        </Container>
+    )
+}
